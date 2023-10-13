76577bb553bf4c88538165195a37a16c7ee843d3
HELPMEEADICE <58381646+HELPMEEADICE@users.noreply.github.com>
2023-10-14 01:02:27 +0800

    Update proxy.py
    Updated asynchronous processing, greatly improving efficiency

--- proxy.py
+++ proxy.py
@@ -9,8 +9,12 @@ proxies = {
     'https': 'socks5://127.0.0.1:7890'
 }
 
-# 根目录
-base_directory = 'your directory'
+# 创建持久的Session对象，共享连接池
+session = requests.Session()
+session.proxies = proxies
+
+# 存储图片的根目录
+base_dir = 'your directory'
 
 @app.route('/', methods=['GET'])
 def root():
@@ -18,35 +22,39 @@ def root():
     response_text = '<p style="font-family: Arial;">Pixiv图片反代服务器</p>'
     return Response(response_text, status=200, content_type='text/html;charset=UTF-8')
 
-@app.route('/<path:subpath>', methods=['GET'])
-def proxy(subpath):
-    # 构建本地文件路径
-    local_filename = os.path.join(base_directory, subpath)
+@app.route('/<path:path>', methods=['GET'])
+def proxy(path):
+    # 获取本地文件路径
+    local_file_path = os.path.join(base_dir, path)
 
-    # 检查本地文件是否存在
-    if os.path.exists(local_filename):
+    if os.path.exists(local_file_path):
         # 如果本地文件存在，直接返回本地文件内容
-        with open(local_filename, 'rb') as f:
-            content = f.read()
-        return Response(content, status=200, content_type='image/jpeg')  # 这里的content_type需要根据实际文件类型设置
+        with open(local_file_path, 'rb') as file:
+            return Response(file.read(), status=200, content_type='image/jpeg')  # 请根据实际文件类型设置Content-Type
 
-    # 如果本地文件不存在，发送代理请求，使用SOCKS5代理（127.0.0.1:7890）
-    target_url = f'https://i.pximg.net/{subpath}'
-    response = requests.get(target_url, headers={
-        'Referer': 'https://www.pixiv.net/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
-    }, proxies=proxies)
+    # 本地文件不存在，从i.pximg.net获取文件
+    target_url = f'https://i.pximg.net/{path}'  # 拼接目标URL，根据请求路径生成目标URL
 
-    # 构建响应对象
-    flask_response = Response(response.content, status=response.status_code, headers=dict(response.headers))
+    try:
+        # 发送代理请求，使用共享的Session对象
+        response = session.get(target_url, headers={
+            'Referer': 'https://www.pixiv.net/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+        }, timeout=10)
 
-    # 如果代理请求成功，保存文件到本地目录
-    if response.status_code == 200:
-        os.makedirs(os.path.dirname(local_filename), exist_ok=True)
-        with open(local_filename, 'wb') as f:
-            f.write(response.content)
+        if response.status_code == 200:
+            # 保存文件到本地
+            os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+            with open(local_file_path, 'wb') as file:
+                file.write(response.content)
 
-    return flask_response
+        # 构建响应对象
+        flask_response = Response(response.content, status=response.status_code, headers=dict(response.headers))
+        return flask_response
+    except Exception as e:
+        # 处理异常情况，例如超时等
+        print(f"Error: {e}")
+        return Response(status=500)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
